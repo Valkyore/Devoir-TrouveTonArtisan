7839fee50fdd50c54ea6601c0e61e769742361e0
Valkyore <alexmille@outlook.fr>
2025-06-23 22:08:11 +0200

    création des pages arstianList et artisanDetail

---
+++ src/app/pages/artisan-liste/artisan-liste.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-artisan-liste',
+  standalone: false,
+  templateUrl: './artisan-liste.component.html',
+  styleUrl: './artisan-liste.component.scss'
+})
+export class ArtisanListeComponent {
+
+}
